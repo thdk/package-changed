7e286d14e5bebe64383c4cb600c804399b215eba
thdk <t.dekiere@gmail.com>
2020-11-07 17:08:06 +0100

    Throw error if package.json is not found

--- src/is-package-changed.ts
+++ src/is-package-changed.ts
@@ -5,15 +5,14 @@ import { getPackageHash } from './get-package-hash';
 
 const isPackageChanged = ({ hashFilename = '.packagehash' } = {}) => {
     const packagePath = findPackage();
+    const writeHash = (hash: string | undefined) => hash && fs.writeFileSync(packageHashPath, hash);
+
     if (!packagePath) {
-        console.error('Cannot find package.json. Travelling up from current working directory.');
-        return undefined;
+        throw new Error('Cannot find package.json. Travelling up from current working directory.');
     }
 
     const packageHashPath = path.join(path.dirname(packagePath), hashFilename);
     const recentDigest = getPackageHash(packagePath);
-
-    const writeHash = (hash: string | undefined) => hash && fs.writeFileSync(packageHashPath, hash);
 
     // if the hash file doesn't exist
     // or if it does and the hash is different
